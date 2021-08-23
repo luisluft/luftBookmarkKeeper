23afb9897d369c751f299378111741842fd9d3f7
luisluft <luberluft@gmail.com>
2021-08-23 09:48:26 -0400

    add data validation on add bookmark modal

--- script.js
+++ script.js
@@ -11,6 +11,32 @@ function showModal() {
   websiteName.focus();
 }
 
+function storeBookmark(event) {
+  event.preventDefault();
+  const nameValue = websiteName.value;
+  let urlValue = websiteUrl.value;
+  if (!urlValue.includes("http://") && !urlValue.includes("https://")) urlvalue = `https://${urlValue}`;
+  if (!validateForm(nameValue, urlValue)) return false;
+}
+
+function validateForm(nameValue, urlValue) {
+  const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert("Please submit values for both fields.");
+    return false;
+  }
+  if (urlValue.match(regex)) {
+    alert("Successful match");
+  } else {
+    alert("Please provide a valid address");
+    return false;
+  }
+  return true;
+}
+
 modalShow.addEventListener("click", showModal);
 modalClose.addEventListener("click", () => modal.classList.remove("show-modal"));
 window.addEventListener("click", (e) => (e.target === modal ? modal.classList.remove("show-modal") : false));
+bookmarkForm.addEventListener("submit", storeBookmark);
